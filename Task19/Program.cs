9f61d77f87efbf862d7b1adb2d64bdb6134d732a
Docvel <docvel@yandex.ru>
2022-12-08 19:44:11 +0300

    fixed task19: desined as method

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,24 +1,35 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное
 // число и проверяет, является ли оно палиндромом.
 // Усложнил задачу.
-Console.Clear();
-Console.Write("Введите число, слово или любой текст без пробела: ");
-string str = Console.ReadLine();
-bool res = int.TryParse(str, out int num);                              // создаём логическую переменную да/нет
-int x = num, y = 0, temp = 0;                                           // временные переменные для расчётов
-if (res == true & x % 10 != 0) {                                        // условие: число целое и не оканчивается на 0
-    for (int i = 0; x > 10; i++) {                                      // запускаем цикл
-        temp = x % 10;
-        y = (y + temp) * 10;
-        x = x / 10;}
-    y = y + x;                                                          // число перевернули
-    if (num == y) Console.Write($"Число {num} - палиндром.");           // сравниваем, выводим результат
-    else Console.Write($"Число {num} - не палиндром.");
-}
-else {
-    char[] strAr = str.ToCharArray();                                   // создаём массив символов из строки
-    Array.Reverse(strAr);                                               // переворачиваем массив
-    string str2 = new String(strAr);                                    // создаём новую строку
-    if (str == str2) Console.Write($"Выражение {str} - палиндром.");    // сравниваем, выводим результат
-    else Console.Write($"Выражение {str} - не палиндром.");
-}
+
+void PalinSearch(string str) {
+    string newstr = str.Replace(" ", "");
+    bool res = int.TryParse(newstr, out int num);                           // создаём логическую переменную да/нет
+    int x = num, y = 0, temp = 0;                                           // временные переменные для расчётов
+    if (res == true & x % 10 != 0) {                                        // условие: число целое и не оканчивается на 0
+        for (int i = 0; x > 10; i++) {                                      // запускаем цикл
+            temp = x % 10;
+            y = (y + temp) * 10;
+            x = x / 10;}
+        y = y + x;                                                          // число перевернули
+        if (num == y) Console.WriteLine($"Число {num} - палиндром.");       // сравниваем, выводим результат
+        else Console.WriteLine($"Число {num} - не палиндром.");
+    }
+    else {
+        char[] strAr = newstr.ToCharArray();                                // создаём массив символов из строки
+        Array.Reverse(strAr);                                               // переворачиваем массив
+        string str2 = new String(strAr);                                    // создаём новую строку
+        if (newstr == str2) Console.WriteLine($"Выражение <{str}> - палиндром.");    // сравниваем, выводим результат
+        else Console.WriteLine($"Выражение <{str}> - не палиндром.");
+    }}
+
+string str1 = "143676341";
+PalinSearch(str1);
+
+string str2 = "0324334230";
+PalinSearch(str2);
+
+string str3 = "A роза упала на лапу Aзора";     // Смена регистра - пока сложно.
+PalinSearch(str3);                              // Вероятно надо использовать тип char
+
+PalinSearch("а роза упала на лапу азора");
